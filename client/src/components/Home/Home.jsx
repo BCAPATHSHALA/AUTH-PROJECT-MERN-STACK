4bf9d3242aa3a0d28f5671c352a2c35a032522d9
BCAPATHSHALA <manojkumargfg.1@gmail.com>
2024-03-03 17:56:09 +0530

    AUTH PROJECT USING MERN STACK

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,20 @@
+import { Flex, Heading, Text } from "@chakra-ui/react";
+
+const Home = () => {
+  return (
+    <>
+      <Flex
+        h={"80vh"}
+        w={"100vw"}
+        flexDirection={"column"}
+        justifyContent={"center"}
+        alignItems={"center"}
+      >
+        <Heading fontSize={["2em", "5em"]}>AUTH PROJECT</Heading>
+        <Text>Login & Signup Portfolio Project</Text>
+      </Flex>
+    </>
+  );
+};
+
+export default Home;
